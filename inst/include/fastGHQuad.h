67309013b259510d2205c0a3cba9c48185bd2b14
Manuel Koller <koller.manuel@gmail.com>
2017-07-15 13:38:09 +0100

    Improving header file to auto load package if required.
    This way fastGHQuad only needs to be added to the other
    packages’ LinkingTo. No need to add it to Depends and
    add dummy imports.

--- inst/include/fastGHQuad.h
+++ inst/include/fastGHQuad.h
@@ -1,24 +1,45 @@
 #ifndef RCPP_fastGHQuad_H_
 #define RCPP_fastGHQuad_H_
 
+#include <R.h>
+#include <Rinternals.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 namespace fastGHQuad {
 
   using namespace Rcpp;
 
-  inline int gaussHermiteDataDirect(int n, std::vector<double>* x, std::vector<double>* w) { 
+  int gaussHermiteDataDirect(int n, std::vector<double>* x, std::vector<double>* w) { 
     static int(*fun)(int, std::vector<double>*, std::vector<double>*) = NULL;
-    if (fun == NULL) fun = (int(*)(int, std::vector<double>*, std::vector<double>*))
-      R_GetCCallable("fastGHQuad","gaussHermiteDataDirect");
+    if (fun == NULL) {
+      Rf_eval(Rf_lang2(Rf_install("loadNamespace"),
+                       Rf_ScalarString(Rf_mkChar("fastGHQuad"))),
+                       R_GlobalEnv);
+      fun = (int(*)(int, std::vector<double>*, std::vector<double>*))
+        R_GetCCallable("fastGHQuad","gaussHermiteDataDirect");
+    }
     return fun(n, x, w);
   }
 
-  inline int gaussHermiteDataGolubWelsch(int n, std::vector<double>* x, std::vector<double>* w) {
+  int gaussHermiteDataGolubWelsch(int n, std::vector<double>* x, std::vector<double>* w) {
     static int(*fun)(int, std::vector<double>*, std::vector<double>*) = NULL;
-    if (fun == NULL) fun =  (int(*)(int, std::vector<double>*, std::vector<double>*))
-      R_GetCCallable("fastGHQuad","gaussHermiteDataGolubWelsch");
+    if (fun == NULL) {
+      Rf_eval(Rf_lang2(Rf_install("loadNamespace"),
+                       Rf_ScalarString(Rf_mkChar("fastGHQuad"))),
+                       R_GlobalEnv);
+      fun =  (int(*)(int, std::vector<double>*, std::vector<double>*))
+        R_GetCCallable("fastGHQuad","gaussHermiteDataGolubWelsch");
+    }
     return fun(n, x, w);
   }
   
 }
+  
+#ifdef __cplusplus
+}
+#endif
 
-#endif // RCPP_fastGHQuad_H_GEN_
+#endif // RCPP_fastGHQuad_H_
