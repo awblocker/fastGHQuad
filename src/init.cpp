1dd11300b153f8ed473eac46f6c9500c37c5fc2a
Manuel Koller <koller.manuel@gmail.com>
2017-07-15 13:55:33 +0100

    Fixing R CMD check NOTE about register symbols.
    As suggested here:
    https://github.com/RcppCore/Rcpp/issues/636#issuecomment-280985661

--- src/init.cpp
+++ src/init.cpp
@@ -3,6 +3,8 @@
 extern "C" {
 
   void R_init_fastGHQuad(DllInfo *info) {
+    R_registerRoutines(info, NULL, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, TRUE);
     R_RegisterCCallable("fastGHQuad", "gaussHermiteDataDirect",
                         (DL_FUNC) &gaussHermiteDataDirect);
     R_RegisterCCallable("fastGHQuad", "gaussHermiteDataGolubWelsch",
